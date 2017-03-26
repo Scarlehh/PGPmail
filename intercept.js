34c07b604bc1a6fe5e85c70a3a16087411aaa93f
Scarlett <gourleys@tcd.ie>
2017-03-26 20:40:54 +0100

    Add button to encrypt and replace text when pressed

--- intercept.js
+++ intercept.js
@@ -1,15 +1,12 @@
-InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk){
-	console.log("test");
+InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 	sdk.Compose.registerComposeViewHandler(function(composeView){
-		// a compose view has come into existence, do something with it!
 		composeView.addButton({
-			title: "My Nifty Button!",
-			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
+			title: "Encrypt",
+			iconClass: "encryptButton",
+			iconUrl: chrome.extension.getURL("encryptButton.png"),
 			onClick: function(event) {
-				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
+				event.composeView.setBodyText("ENCRYPTED");
 			},
 		});
-
 	});
-
 });
