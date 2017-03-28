0367a2c2e1bf6ecc4a5cb7e31b9579b68aae30f8
Scarlett <gourleys@tcd.ie>
2017-03-28 17:51:44 +0100

    Enable button when email in keyring is added

--- intercept.js
+++ intercept.js
@@ -7,6 +7,25 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 			onClick: function(event) {
 				event.composeView.setBodyText("ENCRYPTED");
 			},
+			enabled: false
+		});
+		var encryptButton = document.getElementsByClassName("inboxsdk__button")[0];
+		encryptButton.style = "display: none";
+
+		composeView.on("toContactAdded", function(event) {
+			console.log(event.contact.emailAddress, "added.");
+			chrome.storage.local.get(function(keys) {
+				for(var key in keys) {
+					if(key === event.contact.emailAddress) {
+						encryptButton.style = "";
+					}
+				}
+			});
+		});
+
+		composeView.on("toContactRemoved", function(event) {
+			console.log(event.contact.emailAddress, "removed.");
+			encryptButton.style = "display: none";
 		});
 	});
 });
