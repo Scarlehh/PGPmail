0e2551b20dc61abf502666d9dd04bb8b35c775a5
Scarlett <gourleys@tcd.ie>
2017-03-30 18:45:06 +0100

    Backend for encryption/decryption of multiple recipients

--- intercept.js
+++ intercept.js
@@ -5,21 +5,22 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 			iconClass: "encryptButton",
 			iconUrl: chrome.extension.getURL("resources/images/encryptButton.png"),
 			onClick: function(clickEvent) {
-				var address = composeView.getToRecipients()[0].emailAddress;
-				chrome.storage.local.get(address, function(key) {
-					for(var add in key) {
-						console.log(clickEvent);
-						encrypt(composeView.getHTMLContent(), key[add].pubKey)
-							.then(function(ciphertext) {
-								var html = "";
-								var lines = ciphertext.data.split("\n");
-								for(line in lines) {
-									html += lines[line] + "<br>";
-								}
-								composeView.setBodyHTML(html);
-								composeView.send();
-							});
+				var address = composeView.getToRecipients();//[0].emailAddress;
+				chrome.storage.local.get(function(keys) {
+					var pubKeys = [];
+					for(var i = 0; i < address.length; i++) {
+						pubKeys = pubKeys.concat(keys[address[i].emailAddress].pubKey);
 					}
+					encrypt(composeView.getHTMLContent(), pubKeys)
+						.then(function(ciphertext) {
+							var html = "";
+							var lines = ciphertext.data.split("\n");
+							for(line in lines) {
+								html += lines[line] + "<br>";
+							}
+							composeView.setBodyHTML(html);
+							composeView.send();
+						});
 				});
 			}
 		});
@@ -28,7 +29,6 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 		encryptButton.style = "display: none";
 
 		composeView.on("recipientsChanged", function(event) {
-			console.log(event, "added.");
 			chrome.storage.local.get(function(keys) {
 				if(canEncrypt(keys)) {
 					encryptButton.style = "";
@@ -61,35 +61,35 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 		var messageViews = threadView.getMessageViewsAll();
 		for(var i = 0; i < messageViews.length; i++) {
 			messageViews[i].on("load", function(event) {
-				var recipients = event.messageView.getRecipients();
-				for(var j = 0; j < recipients.length; j++) {
-					// If the message is sent to the user logged in
-					if(recipients[j].emailAddress === sdk.User.getEmailAddress()) {
-						event.messageView.addToolbarButton({
-							section: sdk.Conversations.MessageViewToolbarSectionNames.MORE,
-							title: "Decrypt",
-							iconClass: "decryptButton",
-							iconUrl: chrome.extension.getURL("resources/images/encryptButton.png"),
-							onClick: function(clickEvent) {
-								var message = event.messageView.getBodyElement();
-								var rawMessage = message.textContent || message.innerText || "";
-								var isEncrypted = /-----BEGIN PGP MESSAGE-----(.*\n)*-----END PGP MESSAGE-----/g;
-								// If the message is encrypted
-								if(isEncrypted.test(rawMessage)) {
-									chrome.storage.local.get(sdk.User.getEmailAddress(), function(keys) {
-										var passphrase = prompt("Enter your passphrase");
-										var key = keys[sdk.User.getEmailAddress()];
-										decryptKey(key["privKey"], passphrase).then(function(decryptedKey) {
-											decrypt(rawMessage, decryptedKey).then(function(plaintext) {
-												document.getElementById(message.id).firstChild.firstChild.innerHTML = plaintext.data;
-											});
-										});
+				event.messageView.addToolbarButton({
+					section: sdk.Conversations.MessageViewToolbarSectionNames.MORE,
+					title: "Decrypt",
+					iconClass: "decryptButton",
+					iconUrl: chrome.extension.getURL("resources/images/encryptButton.png"),
+					onClick: function(clickEvent) {
+						var message = event.messageView.getBodyElement();
+						var rawMessage = message.textContent || message.innerText || "";
+						var isEncrypted = /-----BEGIN PGP MESSAGE-----(.*\n)*-----END PGP MESSAGE-----/g;
+						// If the message is encrypted
+						if(isEncrypted.test(rawMessage)) {
+							chrome.storage.local.get(sdk.User.getEmailAddress(), function(keys) {
+								var passphrase = prompt("Enter your passphrase");
+								var key = keys[sdk.User.getEmailAddress()];
+								decryptKey(key["privKey"], passphrase).then(function(decryptedKey) {
+									console.log("decrypting key");
+									decrypt(rawMessage, decryptedKey).then(function(plaintext) {
+										var html = "";
+										var lines = plaintext.data.split("\n");
+										for(line in lines) {
+											html += lines[line] + "<br>";
+										}
+										document.getElementById(message.id).firstChild.innerHTML = html;
 									});
-								}
-							}
-						});
+								});
+							});
+						}
 					}
-				}
+				});
 			});
 		}
 	});
