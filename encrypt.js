5ae8a9c413025d84c67a3e6e2d7bfc45502a5138
Scarlett <gourleys@tcd.ie>
2017-04-01 16:56:37 +0100

    Change decrypt to also verify

--- encrypt.js
+++ encrypt.js
@@ -11,15 +11,6 @@ function encrypt(message, publicKeys, decryptedKey) {
 	return openpgp.encrypt(options);
 }
 
-function verify(message, publicKey) {
-	var options = {
-		message: openpgp.cleartext.readArmored(message),
-		publicKeys: openpgp.key.readArmored(publicKey).keys[0]
-	};
-
-	return openpgp.verify(options);
-}
-
 function decryptKey(privateKey, password) {
 	var options = {
 		privateKey: openpgp.key.readArmored(privateKey).keys[0],
@@ -29,9 +20,10 @@ function decryptKey(privateKey, password) {
 	return openpgp.decryptKey(options);
 }
 
-function decrypt(cipher, decryptedKey) {
+function decrypt(cipher, publicKey, decryptedKey) {
 	options = {
 		message: openpgp.message.readArmored(cipher),
+		publicKeys: openpgp.key.readArmored(publicKey).keys[0],
 		privateKey: decryptedKey.key
 	};
 
