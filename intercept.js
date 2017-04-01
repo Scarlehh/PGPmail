1ac9462e2cd4f85c4c819573c7366b5d20a996a9
Scarlett <gourleys@tcd.ie>
2017-04-01 15:33:49 +0100

    Add password prompt as function and use to sign messages

--- intercept.js
+++ intercept.js
@@ -1,4 +1,44 @@
 InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
+	function passwordPrompt(callback) {
+		var div = document.createElement("div");
+		div.id = "passphrasePopup";
+		div.style = "z-index: 99; \
+                     width: 200px; \
+                     height: 80px; \
+                     padding: 10px 10px 30px 10px; \
+                     background-color: #f1f1f1; \
+                     background-image: -webkit-linear-gradient(top,#f5f5f5,#f1f1f1); \
+                     position: absolute; \
+                     top: 50%; \
+                     left: 50%; \
+                     margin: -70px 0 0 -170px; \
+                     display: block; \
+                     border: 1px solid rgba(0,0,0,0.1);"
+
+		var pwDiv = document.createElement("h3");
+		pwDiv.style = "margin: 5px";
+		pwDiv.innerHTML = "Enter Passphrase";
+
+		var input = document.createElement("input");
+		input.id="pgpPassphrase";
+		input.className = "gbqfif";
+		input.style = "border: 1px solid rgba(0,0,0,0.1); \
+                       margin: 5px; \
+                       z-index: 6; \
+                       left: 0px;";
+		input.type = "password";
+
+		var button = document.createElement("button");
+		button.innerHTML = "Done";
+		button.className = "T-I T-I-KE";
+		button.style = "margin: 5px";
+		button.onclick = callback;
+		div.appendChild(pwDiv);
+		div.appendChild(input);
+		div.appendChild(button);
+		document.body.appendChild(div);
+	}
+
 	sdk.Compose.registerComposeViewHandler(function(composeView) {
 		composeView.addButton({
 			title: "Secure Send",
@@ -11,16 +51,25 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 					for(var i = 0; i < address.length; i++) {
 						pubKeys = pubKeys.concat(keys[address[i].emailAddress].pubKey);
 					}
-					encrypt(composeView.getHTMLContent(), pubKeys)
-						.then(function(ciphertext) {
-							var html = "";
-							var lines = ciphertext.data.split("\n");
-							for(line in lines) {
-								html += lines[line] + "<br>";
-							}
-							composeView.setBodyHTML(html);
-							composeView.send();
-						});
+
+					passwordPrompt(function() {
+						var passphrase = document.getElementById("pgpPassphrase").value;
+						document.getElementById("passphrasePopup").remove();
+
+						decryptKey(keys[sdk.User.getEmailAddress()].privKey, passphrase)
+							.then(function(decryptedKey) {
+								encrypt(composeView.getHTMLContent(), pubKeys, decryptedKey)
+									.then(function(ciphertext) {
+									    var html = "";
+									    var lines = ciphertext.data.split("\n");
+									    for(line in lines) {
+									    	html += lines[line] + "<br>";
+									    }
+									    composeView.setBodyHTML(html);
+									    composeView.send();
+									});
+							});
+					});
 				});
 			}
 		});
@@ -73,39 +122,7 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 						// If the message is encrypted
 						if(isEncrypted.test(rawMessage)) {
 							chrome.storage.local.get(sdk.User.getEmailAddress(), function(keys) {
-								//var passphrase = prompt("Enter your passphrase");
-								var div = document.createElement("div");
-								div.id = "passphrasePopup";
-								div.style = "width: 200px; \
-                                             height: 80px; \
-                                             padding: 10px 10px 30px 10px; \
-                                             background-color: #f1f1f1; \
-                                             background-image: -webkit-linear-gradient(top,#f5f5f5,#f1f1f1); \
-                                             position: absolute; \
-                                             top: 50%; \
-                                             left: 50%; \
-                                             margin: -70px 0 0 -170px; \
-                                             display: block; \
-                                             border: 1px solid rgba(0,0,0,0.1);"
-
-								var pwDiv = document.createElement("h3");
-								pwDiv.style = "margin: 5px";
-								pwDiv.innerHTML = "Enter Passphrase";
-
-								var input = document.createElement("input");
-								input.id="pgpPassphrase";
-								input.className = "gbqfif";
-								input.style = "border: 1px solid rgba(0,0,0,0.1); \
-                                               margin: 5px; \
-                                               z-index: 6; \
-                                               left: 0px;";
-								input.type = "password";
-
-								var button = document.createElement("button");
-								button.innerHTML = "Done";
-								button.className = "T-I T-I-KE";
-								button.style = "margin: 5px";
-								button.onclick = function() {
+								passwordPrompt(function() {
 									var passphrase = document.getElementById("pgpPassphrase").value;
 									document.getElementById("passphrasePopup").remove();
 
@@ -120,12 +137,7 @@ InboxSDK.load('1.0', 'sdk_PGPmail_b4932b6799').then(function(sdk) {
 											document.getElementById(message.id).firstChild.innerHTML = html;
 										});
 									});
-								};
-
-								div.appendChild(pwDiv);
-								div.appendChild(input);
-								div.appendChild(button);
-								document.body.appendChild(div);
+								});
 							});
 						}
 					}
